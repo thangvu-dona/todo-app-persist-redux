617c49fefbdd4775ff8d6fdbf2b50588ce2117af
Thang Vu <thangvu.dona@gmail.com>
2023-11-16 10:15:37 +0700

    Persist Data with redux-persist
    - data no lost when refresh page
    - save data to localStorage technique by redux-persist
    - persistStore, persistReducer
    - PersistGate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit"
 import todoReducer from "../components/TodoList/todoListSlice";
 import filtersReducer from "../components/Filters/FiltersSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
-const store = configureStore({
-  reducer: {
-    todoList: todoReducer,
-    filters: filtersReducer,
-  }
-});
+const rootReducer = combineReducers({
+  todoList: todoReducer,
+  filters: filtersReducer,
+})
 
-export default store
+const persistConfig = {
+  key: 'root',
+  storage, // refer to localStorage
+  stateReconciler: autoMergeLevel2
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store);
+
+// Redux toolkit for create store without persist
+// const store = configureStore({
+//   reducer: {
+//     todoList: todoReducer,
+//     filters: filtersReducer,
+//   }
+// });
+
+// export default store
